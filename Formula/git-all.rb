0e50fa9e323bc4f5c5839733aead30bd42efd723
Rob Sanheim <rsanheim@gmail.com>
2026-02-10 03:25:15 -0600

    Release 0.6.1

--- Formula/git-all.rb
+++ Formula/git-all.rb
@@ -1,17 +1,17 @@
 class GitAll < Formula
   desc "Parallel git operations across many repositories"
   homepage "https://github.com/rsanheim/git-all"
-  version "0.6.0"
+  version "0.6.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/rsanheim/git-all/releases/download/v#{version}/git-all-#{version}-darwin-arm64.tar.gz"
-      sha256 "71bf9ab4cff360ff21590e25e8158cfe1c7fbf11dc2776625f88f1b9e00790bf"
+      sha256 "d7d403b0c67a75e6f8790a802725185523a064ef7dd73df107956481fdbb7f41"
     end
     on_intel do
       url "https://github.com/rsanheim/git-all/releases/download/v#{version}/git-all-#{version}-darwin-x86_64.tar.gz"
-      sha256 "522356a96ce428d1fc46220898b04a19b51f37fa175a18be76d236d0f9ec630c"
+      sha256 "62cd2da1642d18f626306e2c3c53454344798c66d2fd56e45ddb87745d342595"
     end
   end
 
