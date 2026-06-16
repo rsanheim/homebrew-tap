0f899603336f28f3d4d689c0c81450c16b02bf5b
Rob Sanheim <rsanheim@gmail.com>
2026-06-16 04:21:10 -0500

    git-all 0.7.2

--- Formula/git-all.rb
+++ Formula/git-all.rb
@@ -1,28 +1,28 @@
 class GitAll < Formula
   desc "Parallel git operations across many repositories"
   homepage "https://github.com/rsanheim/git-all"
-  version "0.7.1"
+  version "0.7.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/rsanheim/git-all/releases/download/v#{version}/git-all-#{version}-darwin-arm64.tar.gz"
-      sha256 "00d24ebbfaa9890906dce74e0913c34cbca0a3a60854878c02e0471fe4044b63"
+      sha256 "0892b7cd889efa97f7d91bcbc11aebe5602660e95179c715e46da8f8fa301532"
     end
     on_intel do
       url "https://github.com/rsanheim/git-all/releases/download/v#{version}/git-all-#{version}-darwin-x86_64.tar.gz"
-      sha256 "ab02a6df47cb97f23f7810ca1f22b2d678e6e59bfcff7e617f7358c2967e1b0d"
+      sha256 "e3bfdc3a77c40277981e689d68f3d6e1c8a7412c3d5ac5e0de89117ec47b62b5"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/rsanheim/git-all/releases/download/v#{version}/git-all-#{version}-linux-arm64.tar.gz"
-      sha256 "e7b86bc898d4fb7fa44411f7df630564bcb4001ed3e03e98bda0dd60347765bc"
+      sha256 "0ff8765fcac53b636d3ed0a778bb1321296646b6685d9b8afd5a936822e61d6d"
     end
     on_intel do
       url "https://github.com/rsanheim/git-all/releases/download/v#{version}/git-all-#{version}-linux-x86_64.tar.gz"
-      sha256 "90192d83b16c17c49552891dcdf285df88db6731946ec5ba99c10e801515ee4e"
+      sha256 "2e272d0fc26d65bd907606ef3f2583c9735a8a257506bd00127a0fc3cda522fd"
     end
   end
 
